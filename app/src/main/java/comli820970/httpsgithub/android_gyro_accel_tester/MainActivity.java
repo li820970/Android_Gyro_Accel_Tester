b89a3629f802d794c54eacd94e3028c44872cbe6
Colin Li <li820970@gmail.com>
2015-10-12 21:28:03 -0500

    Initial commit

---
+++ app/src/main/java/comli820970/httpsgithub/android_gyro_accel_tester/MainActivity.java
@@ -0,0 +1,13 @@
+package comli820970.httpsgithub.android_gyro_accel_tester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
